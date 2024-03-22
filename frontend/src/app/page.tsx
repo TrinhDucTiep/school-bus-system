6bf5073df93b5fa9b7cc991eb18bdf8d39d3cb96
TrinhDucTiep <nhungthisope123@gmail.com>
2024-03-22 22:04:48 +0700

    fix: fix use localstorage not defined

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import * as React from "react";
 import { Auth } from "@/components";
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  if (localStorage.getItem('accessToken') && localStorage.getItem('refreshToken')) {
+  const [accessToken, setAccessToken] = React.useState<string | null>();
+  const [refreshToken, setRefreshToken] = React.useState<string | null>();
+
+  React.useEffect(() => {
+    setAccessToken(localStorage.getItem('accessToken'));
+    setRefreshToken(localStorage.getItem('refreshToken'));
+  }, []);
+
+  if (accessToken && refreshToken) {
     return (
       <main className="overflow-hidden">
         <Auth />
