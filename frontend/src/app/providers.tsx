aaeeb3ce9b1d53b8439b92de6ff3e57dd8b1063a
hieudt <danghieu14th@gmail.com>
2024-03-23 12:51:03 +0700

    feat: add tanstack

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -3,6 +3,9 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -10,12 +13,17 @@ export interface ProvidersProps {
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-
-    return (
+  const queryClient = new QueryClient();
+  return (
+    <SessionProvider>
       <NextUIProvider>
         <NextThemesProvider defaultTheme="system" attribute="class" {...themeProps}>
-          {children}
+          <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </NextThemesProvider>
       </NextUIProvider>
-    );
+    </SessionProvider>
+  );
 }
