dbebdb150b4e3c470b2dd53318163719b2454970
hieudt <danghieu14th@gmail.com>
2024-04-12 22:21:12 +0700

    feat: fix missing

--- backend/api/src/main/java/com/example/api/controllers/client/ClientPickupPointController.java
+++ backend/api/src/main/java/com/example/api/controllers/client/ClientPickupPointController.java
@@ -1,11 +1,7 @@
 package com.example.api.controllers.client;
 
-import com.example.api.controllers.admin.dto.AddPickupPointRequest;
-import com.example.api.controllers.admin.dto.DeletePickupPointRequest;
 import com.example.api.controllers.admin.dto.PickupPointFilterParam;
-import com.example.api.controllers.admin.dto.UpdatePickupPointRequest;
 import com.example.api.services.account.AccountService;
-import com.example.api.services.account.AccountServiceImpl;
 import com.example.api.services.pickup_point.PickupPointService;
 import com.example.shared.db.entities.Account;
 import com.example.shared.response.CommonResponse;
@@ -16,11 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,15 +25,14 @@ public class ClientPickupPointController {
     private final AccountService accountService;
 
     @GetMapping("/pagination")
-    public ResponseEntity<CommonResponse<Object>> getPickupPoints(PickupPointFilterParam filterParam,
-                                                                  Integer page, Integer size,
-                                                                  String sort,
-                                                                  @AuthenticationPrincipal Account account) {
+    public ResponseEntity<CommonResponse<Object>> getPickupPoints(
+        PickupPointFilterParam filterParam,
+        Integer page, Integer size,
+        String sort,
+        @AuthenticationPrincipal Account account) {
         Pageable pageable = PageableUtils.generate(page, size, sort);
-        Long parentId = accountService.getParentDetail(account).getId();
-        filterParam.setParentId(parentId);
         return ResponseUtil.toSuccessCommonResponse(
-                pickupPointService.getListStudentPickupPoint(account, pageable)
+            pickupPointService.getListStudentPickupPoint(account, pageable)
         );
     }
 
